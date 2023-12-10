eca053eb5721e732e32ba2215eb95d28413c1ebd
romys <romysaputrasihanandaa@gmail.com>
2023-12-10 18:13:58 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+from json import dumps
+from datetime import datetime
+
+print(datetime.fromtimestamp(1677419831))
+
+
+# req = requests.get('https://www.tiktok.com/api/comment/list/?aid=1988&aweme_id=7170139292767882522&count=9999999&cursor=0')
+
+# with open('file.json', 'w') as file:
+#     file.write(dumps(req.json(), ensure_ascii=False))
